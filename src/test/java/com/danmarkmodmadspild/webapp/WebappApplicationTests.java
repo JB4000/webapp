8aac1a939648b560bd24cf217453270af25c1332
JB4000 <jabi0002@stud.ek.dk>
2025-10-27 22:27:22 +0100

    Tilpasset .gitignore for at skabe færre merge konflikter

---
+++ src/test/java/com/danmarkmodmadspild/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danmarkmodmadspild.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
